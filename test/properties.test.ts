17df5cbaa4fa1e29989cdfe1a83553049c6ef1c7
Aditya170700 <adityaric72@gmail.com>
2024-06-13 06:13:16 +0700

    Method

--- test/properties.test.ts
+++ test/properties.test.ts
@@ -8,11 +8,21 @@ describe('Class', () => {
             this.id = id;
             this.name = name;
         }
+
+        sayHello(name: string): void {
+            console.log(`Hello ${name}, my name is ${this.name}`);
+        }
     }
 
     it('Should can add properties', () => {
         const customer: Customer = new Customer(1, 'Aditya');
 
         console.log(customer);
+    });
+
+    it('Should can add method', () => {
+        const customer: Customer = new Customer(1, 'Aditya');
+
+        customer.sayHello('Erni Safitri');
     });
 });
